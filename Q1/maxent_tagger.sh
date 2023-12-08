8478134ff70859cf52590ba643457a2158d934a1
Maria Paula Cortes <mpcortes@patas.ling.washington.edu>
2023-12-07 20:56:35 -0800

    added q1 files

---
+++ Q1/maxent_tagger.sh
@@ -0,0 +1,6 @@
+#maxent_tagger.sh train_file test_file rare_thres feat_thres output_dir
+
+#!/bin/sh
+
+# DO NOT change the path of the interpreter in your homework
+/nopt/dropbox/23-24/570/envs/570/bin/python maxent_tagger.py "$1" "$2" 
